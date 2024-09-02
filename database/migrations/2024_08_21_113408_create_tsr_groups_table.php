0ba05e6c272e2e6869c36d11e9567fb9cd5e1b7a
kradworkz <80160702+kradworkz@users.noreply.github.com>
2024-09-02 09:12:45 +0800

    adsas

--- database/migrations/2024_08_21_113408_create_tsr_groups_table.php
+++ database/migrations/2024_08_21_113408_create_tsr_groups_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('tsr_groups', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->bigIncrements('id');
-            $table->integer('group');
             $table->integer('days');
             $table->integer('quantity');
             $table->decimal('fee',12,2);
