c07257d2655796ef17b136aec4ed25609dc93084
kradworkz <80160702+kradworkz@users.noreply.github.com>
2024-09-02 09:15:33 +0800

    ads

--- database/seeders/ConfigurationsTableSeeder.php
+++ database/seeders/ConfigurationsTableSeeder.php
@@ -15,7 +15,7 @@ class ConfigurationsTableSeeder extends Seeder
     {
         
 
-        \DB::table('configurations')->truncate();
+        \DB::table('configurations')->delete();
         
         \DB::table('configurations')->insert(array (
             0 => 
@@ -23,13 +23,13 @@ class ConfigurationsTableSeeder extends Seeder
                 'id' => 1,
                 'acronym' => 'DOST-IX',
                 'name' => 'Department of Science and Technology - IX',
-                'laboratories' => '[{"name": "Chemical Laboratory", "value": 1, "tsr_count": "393", "report_count": "847", "sample_count": "993"}, {"name": "Microbiological Laboratory", "value": 2, "tsr_count": "339", "report_count": "928", "sample_count": "929"}, {"name": "Metrology Laboratory", "value": 3, "tsr_count": "426", "report_count": "732", "sample_count": "1513"}]',
+                'laboratories' => '[{"name": "Chemical Laboratory", "value": 1, "tsr_count": "389", "report_count": "847", "sample_count": "989"}, {"name": "Microbiological Laboratory", "value": 2, "tsr_count": "336", "report_count": "928", "sample_count": "926"}, {"name": "Metrology Laboratory", "value": 3, "tsr_count": "426", "report_count": "732", "sample_count": "1513"}]',
                 'samplecode_year' => 0,
                 'tsr_count' => NULL,
                 'sample_count' => NULL,
                 'laboratory_id' => 14,
-                'created_at' => '2024-08-14 13:49:43',
-                'updated_at' => '2024-08-14 13:49:43',
+                'created_at' => '2024-08-19 08:42:14',
+                'updated_at' => '2024-08-19 08:42:14',
             ),
         ));
 
