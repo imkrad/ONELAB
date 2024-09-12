03c731ea31c3135f1bf321fc85f4958ac4c19dee
kradworkz <80160702+kradworkz@users.noreply.github.com>
2024-09-12 16:39:37 +0800

    asda

--- app/Services/Laboratory/Tsr/ViewClass.php
+++ app/Services/Laboratory/Tsr/ViewClass.php
@@ -239,7 +239,7 @@ class ViewClass
         $lab = json_decode($tsr);
     
         $head = UserRole::with('user:id','user.profile:id,user_id,firstname,middlename,lastname')
-        ->where('laboratory_id',$lab->laboratory_id)->where('laboratory_type',$labcolor->lab_type->id)->whereHas('role',function ($query){
+       ->where('laboratory_type',$labcolor->lab_type->id)->whereHas('role',function ($query){
             $query->where('name','Technical Manager');
         })->first();
 
